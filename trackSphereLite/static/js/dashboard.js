89887b107c1e26d00dbf6f03bf3c57e4b4ed8d0c
Jihun Park <jihunpark0989@gmail.com>
2025-02-02 22:19:54 +0000

    refactoring

--- trackSphereLite/static/js/dashboard.js
+++ trackSphereLite/static/js/dashboard.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   selected = $("textarea#selected_csv").val(); // selects hidden text field with a csv of metric_ids
-  url = get_url() + "/api/metrics";
+  url = "/api/metrics";
   data = JSON.stringify({ metric_id_list: selected });
   req = post(url, data);
   req.success((json) => {
